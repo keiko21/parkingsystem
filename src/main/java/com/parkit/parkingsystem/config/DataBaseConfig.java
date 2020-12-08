64bc196dddb47418a46865d9d6229d4712a7ef2e
Anastaciya Migal <42586471+keiko21@users.noreply.github.com>
2020-12-07 19:45:40 -0500

    Improve security of database

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -3,26 +3,44 @@ package com.parkit.parkingsystem.config;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    private static final String PROPERTIES_PATH = "src/main/resources/database.properties";
+    private String url;
+    private String user;
+    private String password;
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
+
+        try (FileInputStream fileInputStream = new FileInputStream(PROPERTIES_PATH)) {
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+
+            url = properties.getProperty("url");
+            user = properties.getProperty("user");
+            password = properties.getProperty("password");
+
+        } catch (IOException e) {
+            logger.error("Cannot write into" + PROPERTIES_PATH, e);
+        }
+        return DriverManager.getConnection(url, user, password);
     }
 
-    public void closeConnection(Connection con){
-        if(con!=null){
+    public void closeConnection(Connection con) {
+        if (con != null) {
             try {
                 con.close();
                 logger.info("Closing DB connection");
             } catch (SQLException e) {
-                logger.error("Error while closing connection",e);
+                logger.error("Error while closing connection", e);
             }
         }
     }
